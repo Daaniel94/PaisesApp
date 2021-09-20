1c8eafe2936d084916bc2d689c6b75b514e46563
Luis Daniel Guerrero Ramirez <luisdanielgr94@gmail.com>
2021-09-20 14:27:41 -0500

    PorRegion component

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
+    `button{
+      margin-right: 5px;
+    }`
   ]
 })
 export class PorRegionComponent implements OnInit {
 
-  constructor() { }
+  regiones : string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva : string = '';
+  paises : Country[] = []
+  
+  constructor(
+    private paisServicio : PaisService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  getClaseCSS( region : string ){
+    return( region === this.regionActiva ) ? 'btn btn-primary' : 'btn btn-outline-primary'
+  }
+
+  activarRegion( region : string ){
+    if( region === this.regionActiva ) { return; }
+    this.regionActiva = region;
+    this.paises = [];
+    this.paisServicio.buscarRegion( region )
+      .subscribe( resp => {
+        this.paises = resp;
+      });
   }
 
 }
